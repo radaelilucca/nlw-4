896de0eb3dd001845b27acb1ec2d537111c2a0c1
Lucca Radaeli <radaelidev@gmail.com>
2021-02-24 22:28:44 -0300

    feat(components/countdown) - add start button

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -5,15 +5,20 @@ import S from "./styles.module.scss";
 const Countdown: React.FC = () => {
   return (
     <div className={S.container}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+      <div className={S.containerClock}>
+        <div>
+          <span>2</span>
+          <span>5</span>
+        </div>
+        <span className={S.spliter}>:</span>
+        <div>
+          <span>0</span>
+          <span>0</span>
+        </div>
       </div>
-      <span className={S.spliter}>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+      <button type="button" className={S.startButton}>
+        Start
+      </button>
     </div>
   );
 };
