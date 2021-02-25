5dc65b068cc0829eff91b967a17009e427b9f49a
Lucca Radaeli <radaelidev@gmail.com>
2021-02-24 23:05:16 -0300

    feat(components/countdown) - finish functionality

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 
 import S from "./styles.module.scss";
 
+let countdownTimeout: NodeJS.Timeout;
+
 const Countdown: React.FC = () => {
   const [timer, setTimer] = useState(60 * 25);
   const [isActive, setActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const [minutesLeft, minutesRight] = String(Math.floor(timer / 60))
     .padStart(2, "0")
@@ -13,13 +16,21 @@ const Countdown: React.FC = () => {
     .padStart(2, "0")
     .split("");
 
+  const resetCountdown = () => {
+    clearTimeout(countdownTimeout);
+    setActive(false);
+    setTimer(60 * 25);
+  };
+
   useEffect(() => {
     if (isActive && timer > 0) {
-      setTimeout(() => {
+      countdownTimeout = setTimeout(() => {
         setTimer((prevState) => prevState - 1);
-      }, 1000);
-    } else {
+      }, 1);
+    } else if (isActive && timer === 0) {
+      console.log("finished");
       setActive(false);
+      setHasFinished(true);
     }
   }, [isActive, timer]);
 
@@ -36,13 +47,31 @@ const Countdown: React.FC = () => {
           <span>{secondsRight}</span>
         </div>
       </div>
-      <button
-        type="button"
-        className={S.startButton}
-        onClick={() => setActive(true)}
-      >
-        Start
-      </button>
+      {hasFinished ? (
+        <button disabled className={S.startButton}>
+          Ciclo encerrado
+        </button>
+      ) : (
+        <>
+          {!isActive ? (
+            <button
+              type="button"
+              className={S.startButton}
+              onClick={() => setActive(true)}
+            >
+              Iniciar um ciclo
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={`${S.startButton}  ${S.stopButton}`}
+              onClick={resetCountdown}
+            >
+              Abandonar ciclo
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 };
