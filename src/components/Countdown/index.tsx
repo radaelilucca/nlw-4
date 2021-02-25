5d8c874597675d352067e6e71c1b8212d39f7c29
Lucca Radaeli <radaelidev@gmail.com>
2021-02-24 22:41:41 -0300

    feat(components/countdown) - add timer function

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import S from "./styles.module.scss";
 
 const Countdown: React.FC = () => {
+  const [timer, setTimer] = useState(60 * 25);
+  const [isActive, setActive] = useState(false);
+
+  const [minutesLeft, minutesRight] = String(Math.floor(timer / 60))
+    .padStart(2, "0")
+    .split("");
+  const [secondsLeft, secondsRight] = String(timer % 60)
+    .padStart(2, "0")
+    .split("");
+
+  useEffect(() => {
+    if (isActive && timer > 0) {
+      setTimeout(() => {
+        setTimer((prevState) => prevState - 1);
+      }, 1000);
+    } else {
+      setActive(false);
+    }
+  }, [isActive, timer]);
+
   return (
     <div className={S.container}>
       <div className={S.containerClock}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minutesLeft}</span>
+          <span>{minutesRight} </span>
         </div>
         <span className={S.spliter}>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondsLeft}</span>
+          <span>{secondsRight}</span>
         </div>
       </div>
-      <button type="button" className={S.startButton}>
+      <button
+        type="button"
+        className={S.startButton}
+        onClick={() => setActive(true)}
+      >
         Start
       </button>
     </div>
